e778539e8095b7d83aed31b6410fab69d91e3fa4
Ivan Votintsev <ip.votincev@yandex.ru>
2024-03-21 14:08:44 +0300

    Initial commit

---
+++ plugin/my-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: My plugin
+Plugin URI: https://sitekomi.ru
+Description: My test plugin
+Version: 1.0
+Author: Ivan Votintsev
+Author URI: https://sitekomi.ru
+License: GPLv2 or later
+Text Domain: my-theme
+*/
